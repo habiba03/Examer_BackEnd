dca2cec69b926a903410bcc41ab092a002bf0e04
habiba03 <habibamohammedyahia@gmail.com>
2026-02-25 19:44:46 +0200

    final updates

---
+++ src/main/java/com/online_exam/examer/exception/ExamExpiredException.java
@@ -0,0 +1,7 @@
+package com.online_exam.examer.exception;
+
+public class ExamExpiredException extends RuntimeException {
+    public ExamExpiredException(String message) {
+      super(message);
+    }
+}
